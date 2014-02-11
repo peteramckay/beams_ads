02ad6f539b470a6a7ceb3880b06daf33d0c8264f
Peter <peteramckay@gmail.com>
2014-02-10 20:39:04 -0500

    adding some files

--- js/jqm.globals.js
+++
@@ -1,7 +0,0 @@
-$(document).bind('mobileinit', function () {
-    $.mobile.allowCrossDomainPages = true;
-    $.mobile.zoom.enabled = false;
-    $.mobile.buttonMarkup.hoverDelay = 0; //defaults 200
-    $.mobile.defaultDialogTransition = 'none';
-    $.mobile.defaultPageTransition = 'none';
-});
